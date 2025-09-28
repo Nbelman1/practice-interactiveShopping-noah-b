41f80ffd278398f3de040171deda1ccfc3200b11
Noah <nbelman1@gmail.com>
2025-09-28 14:50:14 -0400

    Added files to repo, created HTML elements

---
+++ practice-interactiveShopping-noah-b/script.js
@@ -0,0 +1,20 @@
+
+// 2. Core Features:
+//     ● Add Items:
+//     ○ When the user enters text in the input field and clicks the "Add"
+//     button, a new list item is added to the.
+//     ○ Each list item should include:
+//     ■ The item text.
+//     ■ A "Remove" button to delete the item.
+//     ■ A "Edit" button to modify the item text.
+//     ● Remove Items:
+//     ○ When the "Remove" button is clicked, the corresponding list item is
+//     deleted from the.
+//     ● Edit Items:
+//     ○ When the "Edit" button is clicked:
+//     1
+//     ■ Replace the item text with an input field containing the current
+//     text.
+//     ■ Change the "Edit" button to a "Save" button.
+//     ■ When "Save" is clicked, update the text with the new input value
+//     and revert the button back to "Edit".
