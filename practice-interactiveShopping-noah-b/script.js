0603a0d4815adebf2f68dc3cf72b9c35977b4bf8
Noah <nbelman1@gmail.com>
2025-09-28 15:53:36 -0400

    Added event listener to add item to list

--- practice-interactiveShopping-noah-b/script.js
+++ practice-interactiveShopping-noah-b/script.js
@@ -3,6 +3,22 @@
 //     ● Add Items:
 //     ○ When the user enters text in the input field and clicks the "Add"
 //     button, a new list item is added to the.
+
+    const unorderedList = document.querySelector("ul");
+    const addItemButton = document.getElementById("add-item");
+    const itemField = document.getElementById("item-field"); 
+
+    
+
+    addItemButton.addEventListener("click", function(event) { // clicking the button fires event
+        event.preventDefault(); // stops page reload when button is pressed
+        
+        const listItem = document.createElement("li"); // creates li tag
+        const userInput = itemField.value; // stores user input in userInput variable
+        listItem.innerHTML = userInput; // fills li with user input
+        unorderedList.appendChild(listItem); // makes ul a parent of li
+    });
+
 //     ○ Each list item should include:
 //     ■ The item text.
 //     ■ A "Remove" button to delete the item.
