03d77faaec1525c32e035f56a50838b133bd46a7
Noah <nbelman1@gmail.com>
2025-09-28 18:26:14 -0400

    Created functions for creating a list item and remove item buttons

--- practice-interactiveShopping-noah-b/script.js
+++ practice-interactiveShopping-noah-b/script.js
@@ -6,17 +6,91 @@
 
     const unorderedList = document.querySelector("ul");
     const addItemButton = document.getElementById("add-item");
-    const itemField = document.getElementById("item-field"); 
-
-    
+    const itemField = document.getElementById("item-field");
+    let listItem = ""; // define variables on top of functions for global scope
+    let textSpan = "";
+    let userInput = ""; 
 
     addItemButton.addEventListener("click", function(event) { // clicking the button fires event
         event.preventDefault(); // stops page reload when button is pressed
         
-        const listItem = document.createElement("li"); // creates li tag
-        const userInput = itemField.value; // stores user input in userInput variable
-        listItem.innerHTML = userInput; // fills li with user input
-        unorderedList.appendChild(listItem); // makes ul a parent of li
+        userInput = itemField.value; // stores user input in userInput variable
+
+        function createListItem(userInput) { // create functions to keep code DRY
+            textSpan = document.createElement("span");
+            listItem = document.createElement("li"); 
+            textSpan.innerHTML = userInput; // 
+            listItem.appendChild(textSpan); // makes li a parent of span
+            unorderedList.appendChild(listItem); // makes li a parent of ul
+
+            createRemoveButton(listItem);
+            createEditButton(listItem);
+
+            return listItem;
+        }
+
+        function createRemoveButton(listItem) { 
+            const removeItemButton = document.createElement("button"); // create element
+            removeItemButton.setAttribute("type", "button"); // add attributes
+            removeItemButton.setAttribute("name", "remove-item");
+            removeItemButton.setAttribute("class", "remove-item");
+            removeItemButton.textContent = "Remove Item"; // add label
+            listItem.appendChild(removeItemButton); // append button to list item
+            
+            removeItemButton.addEventListener("click", function() {
+                listItem.remove(); // remove list item from list
+            });
+
+            return removeItemButton; // button retains event listener
+        }
+        
+        // add Edit Item button
+        function createEditButton(listItem) {
+            const editItemButton = document.createElement("button"); // create element
+            editItemButton.setAttribute("type", "button"); // add attributes
+            editItemButton.setAttribute("name", "edit-item");
+            editItemButton.setAttribute("class", "edit-item");
+            editItemButton.textContent = "Edit Item"; // add label
+            listItem.appendChild(editItemButton); // append button to list item
+
+            createEditEventListener(listItem, textSpan, editItemButton);
+            return editItemButton;
+        }
+
+        // TODO: fix edit event listener - button currently does nothing when pressed
+        // TODO: figure out how to incorporate createEditButton and createRemoveButton functions inside of the event handler
+
+        function createEditEventListener(listItem, textSpan, editItemButton) {
+            // when Edit Item button is clicked...
+            editItemButton.addEventListener("click", function() {
+                // create text input for new list item
+                const editButtonInput = document.createElement("input"); // create input
+                listItem = document.createElement("li");
+                editButtonInput.setAttribute("type", "text"); 
+                editButtonInput.setAttribute("name", "edit-input");
+                editButtonInput.setAttribute("placeholder", "Enter new item");
+                textSpan.replaceWith(editButtonInput); // delete text span (which contains list item), insert text input field
+
+                // change the "edit" button to a "save" button
+                const saveItemButton = document.createElement("button");
+                saveItemButton.setAttribute("name", "save-item");
+                saveItemButton.setAttribute("type", "button");
+                saveItemButton.setAttribute("class", "save-item");
+                saveItemButton.textContent = "Save Item";
+                editItemButton.replaceWith(saveItemButton);
+            }
+        )}
+
+            // when Save Item button is clicked...
+            // saveItemButton.addEventListener("click", function() {
+            //     createListItem();
+            //     createRemoveButton();
+            // })
+
+        
+
+        createListItem(userInput);
+        
     });
 
 //     ○ Each list item should include:
